299a04f426403f81eac65f48d6c2e4780da7bc8e
Manuel Volk <manuel.volk@xamla.com>
2019-02-21 15:59:49 +0100

    update __init__

--- src/xamla_vision/__init__.py
+++ src/xamla_vision/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 name = "xamla_vision"
 
-from .ros.stereo_laser_line_node import StereoLaserLineNode
+from .ros import (StereoLaserLineClient,
+                  StereoLaserLineNode)
 
 from .capture_client import (XimeaCaptureClient,
                              GeniCamCaptureClient,
@@ -9,7 +10,5 @@ from .capture_client import (XimeaCaptureClient,
 
 from .crop_box_statistics_client import (CropBoxStatisticsProperties,
                                          CropBoxStatisticsClient)
-
-from .stereo_laser_line_client import StereoLaserLineClient
 
 from .xamla_grid_calibrator import XamlaGridCalibrator
--- src/xamla_vision/ros/__init__.py
+++ src/xamla_vision/ros/__init__.py
@@ -2,3 +2,4 @@
 name = "xamla_vision.ros"
 
 from .stereo_laser_line_client import StereoLaserLineClient
+from .stereo_laser_line_node import StereoLaserLineNode
